67e1d38a21f15c6e4427c874cfeb98f4c8f1f41c
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-08 16:55:53 -0400

    Added views file

---
+++ contacts_ls/server/contacts/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from contacts.models import Contact
+from contacts.serializers import ContactSerializer
+
+class ContactViewSet(viewsets.ModelViewSet):
+    """ViewSet for viewing and editing Contact objects """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer 
