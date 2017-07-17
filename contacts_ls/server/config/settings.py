46d8d95641e4c93d0a49e4b8b3ed3f9ab605d00f
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-17 00:16:19 -0400

    Procfile

--- contacts_ls/server/config/settings.py
+++ contacts_ls/server/config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'contacts',
+    'contacts_ls',
     'phonenumber_field',
     'corsheaders',
 ]
